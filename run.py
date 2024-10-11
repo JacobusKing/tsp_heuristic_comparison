9751228d6020e932d9fe73ed6998c4038710d052
jacobusking-se <167509058+jacobusking-se@users.noreply.github.com>
2024-10-11 14:40:02 +0200

    Changes

--- run.py
+++ run.py
@@ -4,17 +4,18 @@ from solver import exact_solver, jk_solver, full_two_opt
 from evaluate import calculate_tour_distance, plot_tour
 
 name = "Jacobus_King"
-time_limit = 6 # Specify the time limit in seconds that will be provided to the solver
-file = 'st70' # Specify the file name containing the instance to be run
+time_limit = 60 # Specify the time limit in seconds that will be provided to the solver
+file = 'tsp225' # Specify the file name containing the instance to be run
 
 # Import instance
 coords = parse_tsplib(file) # Obtain the coordinates of cities in the instance
 dist_matrix = distance_matrix(coords, coords) # Generate the distance matrix
 
 # Call your solver here to take the distance matrix and time limit in seconds as input and provide a tour with 0-based indexing as output
-tour = jk_solver(dist_matrix, time_limit, nit = 0.00001)
+# tour = jk_solver(dist_matrix, time_limit, nit = 0.00001)
 # tour = exact_solver(dist_matrix, time_limit)
 # print('Starting full 2-opt.')
+# tour = list(range(len(dist_matrix)))
 # tour = full_two_opt(tour, dist_matrix, time_limit*0.1)
 
 # Evaluate your solution
